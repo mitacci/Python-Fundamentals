85de9656fcc514d8740c0a2daaf3817d0c369332
Dimitar Stoyanov <97186457+mitacci@users.noreply.github.com>
2022-01-21 22:05:11 +0200

    Create 05_PalindromeIntegers.py

---
+++ 04-Functions/Exercises/05_PalindromeIntegers.py
@@ -0,0 +1,10 @@
+def palindrome(seq):
+    return True if seq == seq[::-1] else False
+
+
+numbers = input().split(', ')
+result = [palindrome(b) for b in numbers]
+
+
+for i in result:
+    print(i)
